afb22c597d806e46890f0cdd9a2b36f718a9a604
tobyrushton <tobyjrushton@icloud.com>
2024-10-30 20:23:31 +0000

    complete section4

---
+++ section4/app/views.py
@@ -0,0 +1,12 @@
+from flask import render_template, flash
+from app import app
+from .forms import CalculatorForm
+
+@app.route('/calculator', methods=['GET', 'POST'])
+def calculator():
+    form = CalculatorForm()
+    if form.validate_on_submit():
+        flash('Succesfully received form data. %s + %s  = %s'%(form.number1.data, form.number2.data, form.number1.data+form.number2.data))
+    return render_template('calculator.html',
+                           title='Calculator',
+                           form=form)
