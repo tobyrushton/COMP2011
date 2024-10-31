b256eb196cd8fd21fabeec66d920c0ff3e7452e3
tobyrushton <tobyjrushton@icloud.com>
2024-10-31 01:17:21 +0000

    cw1 - created assessment component

--- cw1/app/views.py
+++ cw1/app/views.py
@@ -4,7 +4,12 @@ from .forms import AssessmentForm
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # fetch all completed assessments from db
+    completed = db.session.query(models.Assessment).filter_by(completed=True).all()
+    # fetch all current assessments from db
+    current = db.session.query(models.Assessment).filter_by(completed=False).all()
+
+    return render_template('index.html', completed=completed, current=current)
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
@@ -34,3 +39,35 @@ def new():
     return render_template('new.html', 
                            title="Create New Assessment",
                            form=form)
+
+@app.route('/complete/<int:id>', methods=['POST'])
+def complete(id):
+    # fetch the assessment from the db
+    assessment = db.session.query(models.Assessment).filter_by(id=id).first()
+    # mark the assessment as completed
+    assessment.completed = True
+    # update the db
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/uncomplete/<int:id>', methods=['POST'])
+def uncomplete(id):
+    # fetch the assessment from the db
+    assessment = db.session.query(models.Assessment).filter_by(id=id).first()
+    # mark the assessment as uncompleted
+    assessment.completed = False
+    # update the db
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    # fetch the assessment from the db
+    assessment = db.session.query(models.Assessment).filter_by(id=id).first()
+    # delete the assessment from the db
+    db.session.delete(assessment)
+    db.session.commit()
+
+    return redirect(url_for('index'))
